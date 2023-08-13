d64e21cb9902e11989b8bfa93d1dcdb62d90fee2
dok2412 <DoK2.4.1.2@yandex.ru>
2023-08-13 20:42:41 +0300

    environment assembly file

--- cryptoprotection/cryptopro.py
+++ cryptoprotection/cryptopro.py
@@ -3,21 +3,6 @@ import os
 
 
 
-# используется только при старте приложения для создания криптографических кдлючей
-
-# def save_key(public_key, private_key):
-#     # Save the public_key
-#     with open("public_key.txt", "wb") as public_file:
-#         public_file.write(public_key.save_pkcs1())
-#
-#     # Save the private_key
-#     with open("private_key.txt", "wb") as private_file:
-#         private_file.write(private_key.save_pkcs1())
-
-# public_key1, private_key1 = rsa.newkeys(1024)
-
-
-
 # получение побличного криптоключа
 def load_public_key():
     # Read the public_key
